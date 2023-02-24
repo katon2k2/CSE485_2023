28ed88c40184cae4c5444ab03371ea8d9e4e4e32
Nguyen Thai Son <katon12122002@gmail.com>
2023-02-24 19:20:10 +0700

    push code ket noi mysql

---
+++ includes/connect.php
@@ -0,0 +1,8 @@
+<?php
+//ket noi DataBase
+  $servename = "localhost";
+  $username = "root";
+  $password ="";
+  $dbname= "btth01_cse485";
+  $conn=mysqli_connect($servename,$username,$password,$dbname);
+?>
